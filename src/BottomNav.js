c17572f8d3dbbb6f9d2220a14e6ea55560ae3f37
unknown <pritomsaha24.ps@gmail.com>
2023-01-31 07:04:25 -0800

    bottom navbar item updated

--- src/BottomNav.js
+++ src/BottomNav.js
@@ -3,6 +3,7 @@ import 'reactjs-bottom-navigation/dist/index.css'
 import {AiFillHome} from 'react-icons/ai'
 import {AiOutlineContacts} from 'react-icons/ai'
 import {HiMail} from 'react-icons/hi'
+import {FcAbout} from 'react-icons/fc'
 import React from 'react'
 import { useNavigate } from "react-router-dom"
 import './bottomnav.css'
@@ -14,16 +15,35 @@ const BottomNav = () => {
         {
           title: 'Home',
     
-          icon: <AiFillHome style={{ fontSize: '18px' }} />,
+          icon: <AiFillHome style={{ fontSize: '18px' , color:"#2196f3" , borderRadius:"50%" , background:"#0000" }} />,
     
           activeIcon: <AiFillHome style={{ fontSize: '18px', color: '#fff' }} />,
           onClick:() => navigate('/') 
         },
+        
+        {
+            title: 'About',
+      
+            icon: <FcAbout style={{ fontSize: '18px' , color:"#2196f3" }} />,
+      
+            activeIcon: <FcAbout style={{ fontSize: '18px', color: '#fff' }} />,
+      
+            onClick:() => navigate('/about') 
+          },
+          {
+            title: 'Service',
+      
+            icon: <HiMail style={{ fontSize: '18px' , color:"#2196f3"  }} />,
+      
+            activeIcon: <HiMail style={{ fontSize: '18px', color: '#fff' }} />,
+      
+            onClick:() => navigate('/programs') 
+          },
     
         {
           title: 'Appointment',
     
-          icon: <AiOutlineContacts style={{ fontSize: '18px' }} />,
+          icon: <AiOutlineContacts style={{ fontSize: '18px',  color:"#2196f3"  }} />,
     
           activeIcon: <AiOutlineContacts style={{ fontSize: '18px', color: '#fff' }} />,
           onClick:() => navigate('/appointment') 
@@ -31,15 +51,6 @@ const BottomNav = () => {
     
      
     
-        {
-          title: 'Contact',
-    
-          icon: <HiMail style={{ fontSize: '18px' }} />,
-    
-          activeIcon: <HiMail style={{ fontSize: '18px', color: '#fff' }} />,
-    
-          onClick:() => navigate('/contact') 
-        },
       ] 
   return (
     <div className='bottom-nav'>
